79abc853f80722b247e805cae2aae3cad2619c76
Jonas Weinz <s6jowein@uni-bonn.de>
2019-02-26 14:45:06 +0100

    made it a progressive web app

---
+++ sw.js
@@ -0,0 +1,29 @@
+self.addEventListener('install', function(e) {
+    e.waitUntil(
+      caches.open('your-magic-cache').then(function(cache) {
+        return cache.addAll([
+          '/',
+          '/index.html',
+          '/manifest.json',
+          '/icon.png',
+          '/LICENSE',
+          '/main.js',
+          '/grid.js',
+          '/game_manager.js',
+          '/sidebar.js',
+          '/tile.js',
+          '/README.md',
+          '/site.js',
+          '/style.css',
+        ]);
+      })
+    );
+  });
+  
+  self.addEventListener('fetch', function(event) {
+    event.respondWith(
+      caches.match(event.request).then(function(response) {
+        return response || fetch(event.request);
+      })
+    );
+  });
