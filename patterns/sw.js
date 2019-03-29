b0b3ade17cca58f1b85c728f26c1154e01be2736
Jonas Weinz <s6jowein@uni-bonn.de>
2019-03-29 19:11:21 +0100

    fixed patterns service worker

--- patterns/sw.js
+++ patterns/sw.js
@@ -2,19 +2,19 @@ self.addEventListener('install', function(e) {
     e.waitUntil(
       caches.open('your-magic-cache').then(function(cache) {
         return cache.addAll([
-          'patterns/',
-          'patterns/index.html',
-          'patterns/manifest.json',
-          'patterns/icon.png',
-          'patterns/LICENSE',
-          'patterns/main.js',
-          'patterns/grid.js',
-          'patterns/game_manager.js',
-          'patterns/sidebar.js',
-          'patterns/tile.js',
-          'patterns/README.md',
-          'patterns/site.js',
-          'patterns/style.css',
+          '/patterns/',
+          '/patterns/index.html',
+          '/patterns/manifest.json',
+          '/patterns/icon.png',
+          '/patterns/LICENSE',
+          '/patterns/main.js',
+          '/patterns/grid.js',
+          '/patterns/game_manager.js',
+          '/patterns/sidebar.js',
+          '/patterns/tile.js',
+          '/patterns/README.md',
+          '/patterns/site.js',
+          '/patterns/style.css',
         ]);
       })
     );
