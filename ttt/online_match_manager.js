5c7e56a39e27506c3eb80dc8836aa51328882afe
Jonas Weinz <jo.we93@gmx.de>
2019-03-31 16:36:11 +0200

    fix typo

--- ttt/online_match_manager.js
+++ ttt/online_match_manager.js
@@ -261,7 +261,7 @@ class OnlineMatchManager
             }
             else
             {
-                status_mesage("Game over, you lost!");
+                status_message("Game over, you lost!");
                 control_head += "You lost";
                 this.control_container.set_background_color(this.online_opponent.get_color_with_alpha(0.4));
             }
