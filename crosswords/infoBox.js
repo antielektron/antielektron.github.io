00d85a29064e01965edffe8b2b66a8013bc8e5cf
Jonas Weinz <jo.we93@gmx.de>
2023-03-27 20:26:07 +0200

    Update infoBox.js

--- crosswords/infoBox.js
+++ crosswords/infoBox.js
@@ -37,7 +37,7 @@ export class InfoBox extends LitElement {
                 position: fixed;
                 display: grid;
                 grid-template-columns: auto min-content;
-                bottom: 0em;
+                top: 0em;
                 left: 0em;
                 right: 0em;
                 min-height: 8em;
